4831804fc8e411c9b4c7b69aa3f01fdce41442d9
Ryan Lirio <ryanlirio2@gmail.com>
2024-09-18 14:24:14 -0300

    vamoquerer

---
+++ usandoStruct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product(1, "mouse gamer", 216.90);
+
+            mouse.Id = 99;
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id;
+        public string Name; 
+        public double Price;
+
+        public double PriceinDolar(Double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}
